b0d39fa52f466b3b1eb06a9d8179611d29ee3389
nicole03 <ntidwell03@gmail.com>
2018-07-31 18:44:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/' do 
+"Hello World"
 end
+
+get '/name' do
+"My name is Nicole."
+end 
+
+get '/hometown' do 
+"My hometown is Nashville, Tennessee."
+end
+
+get '/favorite-song' do 
+"My favorite song is Jumpsuit."
+end
+end 
